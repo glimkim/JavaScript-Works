05f1762a161f1d30d09cb61c9d2fe9485d965df4
hellolimi <hellolaimmn@gmail.com>
2021-03-18 01:13:35 +0900

    improve codes

--- MyClock/common.js
+++ MyClock/common.js
@@ -3,28 +3,33 @@ function clock(){
     var clockDate = [today.getFullYear(), today.getMonth() + 1, today.getDate()];
     var clockTime = [today.getHours(), today.getMinutes(), today.getSeconds()];
 
-    var w = today.getDay();
+
+    var today = new Date();
+    var clockDate = [today.getFullYear(), today.getMonth() + 1, today.getDate()];
+    var clockTime = [today.getHours(), today.getMinutes(), today.getSeconds()];
+
+    var weekDay = today.getDay();
     var week = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'];
-    
-    var time = document.getElementsByTagName('h2');
-    var day = document.getElementById('weekday');
-    var date = document.getElementsByTagName('h3');
-    
-    for(var i in date){
-        if(clockDate[i] >= 10){
-            date[i].textContent = clockDate[i];
+
+    var time = document.getElementsByClassName('time');
+    var date = document.getElementsByClassName('date');
+
+    for(var i in time){
+        if(clockTime[i] < 10){
+            time[i].textContent = '0' + clockTime[i];
         }else{
-            date[i].textContent = '0' + clockDate[i];
+            time[i].textContent = clockTime[i];
         }
     }
-    for(var i in time){
-        if(clockTime[i] >= 10){
-            time[i].textContent = clockTime[i];
+ 
+    for(var i in date){
+        if(clockDate[i] < 10){
+            date[i].textContent = '0' + clockDate[i];
         }else{
-            time[i].textContent = '0' + clockTime[i];
-        }  
+            date[i].textContent = clockDate[i];
+        }
     }
-    day.textContent = week[w];
+
 }
 
 clock();
