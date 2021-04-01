09d8a7cc75902b1efe35841a2756bb95dd3e956e
hellolimi <hellolaimmn@gmail.com>
2021-04-01 14:58:11 +0100

    fundamental works done

---
+++ Chatroom_ver1.0/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const socketIO = require('socket.io');
+const path = require('path');
+const moment = require('moment');
+
+const io = socketIO(server);
+
+app.use(express.static(path.join(__dirname, 'src')));
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {console.log(`Server is running at ${PORT}`)});
+
+io.on('connection', (socket) => { //서버쪽
+    socket.on('inAlert', (data) => {
+        const { name } = data;
+        io.emit('inAlert', {
+            name
+        });
+    }); 
+    socket.on('chatting', (data) => {
+        const { name, msg } = data;
+        io.emit('chatting', {
+            name,
+            msg,
+            time: moment(new Date()).format('h:mm A')
+        });
+    }); //chat.js에 사용한 socket 내용 불러오기
+});
---
+++ Chatroom_ver1.0/src/js/chat.js
@@ -0,0 +1,64 @@
+'use strict'
+const socket = io();
+const nickname = document.querySelector('#nickname');
+const nextButton = document.querySelector('#next');
+const select01 = document.querySelector('.select01');
+
+const chatRoom = document.querySelector('.chatRoom');
+const displayContainer = document.querySelector('.displayContainer');
+const chatList = document.querySelector('.chatList');
+const message = document.querySelector('#message');
+const sendButton = document.querySelector('#send');
+
+chatRoom.style.display = 'none';
+
+const param = {}
+
+nextButton.addEventListener('click', () => {
+    param.name = nickname.value;
+    socket.emit('inAlert', param);
+    select01.style.display = 'none';
+    chatRoom.style.display = 'block';
+});
+
+socket.on('inAlert', (data) => {
+    const inAlert = document.createElement('div');
+    inAlert.classList.add('inAlert');
+    inAlert.innerText = `${data.name} joined`;
+    chatList.appendChild(inAlert);
+});
+
+sendButton.addEventListener('click', () => {
+    param.msg = message.value;
+    socket.emit('chatting', param);
+});
+
+function LiModel(name, msg, time){
+    this.name = name;
+    this.msg = msg;
+    this.time = time;
+
+    this.makeLi = () => {
+        const li = document.createElement('li');
+        /* li.classList.add(nickname.value==this.name?'sent':'received'); */
+        const dom = 
+        `
+        <div class="profile">
+           ${this.name}
+        </div>
+        <div class="message">
+            <span class="contents">${this.msg}</span>
+            <span class="time">${this.time}</span>
+        </div>
+        `;
+        li.innerHTML = dom;
+        chatList.appendChild(li);
+    }
+}
+
+socket.on('chatting', (data) => {
+    console.log(data);
+    const {name, msg, time} = data;
+    const aMessage = new LiModel(name, msg, time);
+    aMessage.makeLi();
+});
